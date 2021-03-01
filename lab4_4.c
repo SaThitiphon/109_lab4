aa02bd283e8c6be4c4860071354473c653a81edb
Thitipon <thitiphon.s@ku.th>
2021-03-01 18:41:01 +0700

    gets to scanf

--- lab4_4.c
+++ lab4_4.c
@@ -3,14 +3,16 @@ int main()
 {
     char a[100],b[100];
     int i=0,j=1;
-    gets(a);
+    scanf("%[^\n]c",a);
     b[0] = toupper(a[0]);
     while(a[i]!='\0')
     {
         if(a[i]==' ')
         {
+            
             b[j] = toupper(a[i+1]);
             j++;
+            
         }
         i++;
     }
