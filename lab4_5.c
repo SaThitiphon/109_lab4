82bc2fa3cb1c3ba15285da75ea5c55575c8ee39c
Thitipon <thitiphon.s@ku.th>
2021-03-01 19:43:53 +0700

    Update lab4_5.c

--- lab4_5.c
+++ lab4_5.c
@@ -6,76 +6,78 @@ int main()
     scanf("%d",&n);
     int s[n];
     char r[50];
-    for(i=0;i<n;i++)
+    if(n>0&&n<100)
     {
-        g[4] = (0,0,0,0);
-        g[0] = 1;
-        strcpy(r," ");
-        scanf("%s",r);
-        for(j=0;j<50;j++)
+        for(i=0;i<n;i++)
         {
-            if(r[j]=='\0')
-                break;
-            if(r[j]=='A')
+            g[4] = (0,0,0,0);
+            g[0] = 1;
+            strcpy(r," ");
+            scanf("%s",r);
+            for(j=0;r[j]!='\0';j++)
             {
-                a = g[1];
-                b = g[2];
-                g[1] = b;
-                g[2] = a;
+                if(r[j]=='A')
+                {
+                    a = g[1];
+                    b = g[2];
+                    g[1] = b;
+                    g[2] = a;
+                }
+                else if(r[j]=='B')
+                {
+                    a = g[0];
+                    b = g[3];
+                    g[0] = b;
+                    g[3] = a;
+                }
+                else if(r[j]=='C')
+                {
+                    a = g[0];
+                    b = g[2];
+                    g[0] = b;
+                    g[2] = a;
+                    c = g[1];
+                    d = g[3];
+                    g[1] = d;
+                    g[3] = c;
+                }
+                else if(r[j]=='D')
+                {
+                    a = g[0];
+                    b = g[1];
+                    g[0] = b;
+                    g[1] = a;
+                    c = g[2];
+                    d = g[3];
+                    g[2] = d;
+                    g[3] = c;
+                }
+                else if(r[j]=='E')
+                {
+                    a = g[0];
+                    b = g[3];
+                    g[0] = b;
+                    g[3] = a;
+                    c = g[1];
+                    d = g[4];
+                    g[1] = d;
+                    g[4] = c;
+                }
             }
-            else if(r[j]=='B')
+            for(j=0;j<4;j++)
             {
-                a = g[0];
-                b = g[3];
-                g[0] = b;
-                g[3] = a;
+                if(g[j]==1)
+                {
+                    s[i] = j + 1;
+                }
             }
-            else if(r[j]=='C')
-            {
-                a = g[0];
-                b = g[2];
-                g[0] = b;
-                g[2] = a;
-                c = g[1];
-                d = g[3];
-                g[1] = d;
-                g[3] = c;
-            }
-            else if(r[j]=='D')
-            {
-                a = g[0];
-                b = g[1];
-                g[0] = b;
-                g[1] = a;
-                c = g[2];
-                d = g[3];
-                g[2] = d;
-                g[3] = c;
-            }
-            else if(r[j]=='E')
-            {
-                a = g[0];
-                b = g[3];
-                g[0] = b;
-                g[3] = a;
-                c = g[1];
-                d = g[4];
-                g[1] = d;
-                g[4] = c;
-            }
+            
         }
-        for(j=0;j<4;j++)
+        for(i=0;i<n;i++)
         {
-            if(g[j]==1)
-            {
-                s[i] = j + 1;
-            }
+            printf("%d\n",s[i]);
         }
-        
     }
-    for(i=0;i<n;i++)
-    {
-        printf("%d\n",s[i]);
-    }
+
     return 0;
 }
